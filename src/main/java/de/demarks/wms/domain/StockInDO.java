a35886fd373d12fd2ee1d838864fc65d56a75107
Shouran Mu <shouran.mu@gmail.com>
2019-09-01 14:19:57 +0200

    update

---
+++ src/main/java/de/demarks/wms/domain/StockInDO.java
@@ -0,0 +1,172 @@
+package de.demarks.wms.domain;
+
+
+import java.util.Date;
+
+/**
+ * 入库记录
+ *
+ * @author Shouran
+ */
+public class StockInDO {
+
+    /**
+     * 入库记录
+     */
+    private Integer id;
+
+    /**
+     * 包裹号
+     */
+    private String packet;
+
+    /**
+     * 批次号
+     */
+    private Integer batch;
+
+    /**
+     * 客户ID
+     */
+    private Integer customerID;
+
+    /**
+     * 客户名称
+     */
+    private String customerName;
+
+    /**
+     * 商品ID
+     */
+    private Integer goodID;
+
+    /**
+     * 商品名称
+     */
+    private String goodName;
+
+    /**
+     * 入库仓库ID
+     */
+    private Integer repositoryID;
+
+    /**
+     * 入库数量
+     */
+    private long number;
+
+    /**
+     * 入库日期
+     */
+    private Date time;
+
+    /**
+     * 入库经手人
+     */
+    private String personInCharge;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPacket() {
+        return packet;
+    }
+
+    public void setPacket(String packet) {
+        this.packet = packet;
+    }
+
+    public Integer getBatch() {
+        return batch;
+    }
+
+    public void setBatch(Integer batch) {
+        this.batch = batch;
+    }
+
+    public Integer getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(Integer customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public Integer getGoodID() {
+        return goodID;
+    }
+
+    public void setGoodID(Integer goodID) {
+        this.goodID = goodID;
+    }
+
+    public String getGoodName() {
+        return goodName;
+    }
+
+    public void setGoodName(String goodName) {
+        this.goodName = goodName;
+    }
+
+    public Integer getRepositoryID() {
+        return repositoryID;
+    }
+
+    public void setRepositoryID(Integer repositoryID) {
+        this.repositoryID = repositoryID;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getPersonInCharge() {
+        return personInCharge;
+    }
+
+    public void setPersonInCharge(String personInCharge) {
+        this.personInCharge = personInCharge;
+    }
+
+    @Override
+    public String toString() {
+        return "StockInDO{" +
+                "id=" + id +
+                ", packet='" + packet + '\'' +
+                ", batch=" + batch +
+                ", cusotmerID=" + customerID +
+                ", cusotmerName='" + customerName + '\'' +
+                ", goodID=" + goodID +
+                ", goodName='" + goodName + '\'' +
+                ", repositoryID=" + repositoryID +
+                ", number=" + number +
+                ", time=" + time +
+                ", personInCharge='" + personInCharge + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/de/demarks/wms/StockInMapperTest.java
@@ -0,0 +1,37 @@
+package de.demarks.wms;
+
+import de.demarks.wms.dao.StockInMapper;
+import de.demarks.wms.domain.StockInDO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Date;
+
+@ContextConfiguration(locations = {"classpath:config/SpringApplicationConfiguration.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration("web")
+public class StockInMapperTest{
+
+    @Autowired
+    private StockInMapper stockInMapper;
+
+    @Test
+    public void insertTest() throws Exception {
+
+        StockInDO stockInDO = new StockInDO();
+        stockInDO.setPacket("DHL");
+        stockInDO.setBatch(10);
+        stockInDO.setGoodID(103);
+        stockInDO.setCustomerID(1214);
+        stockInDO.setNumber(100);
+        stockInDO.setPersonInCharge("admin");
+        stockInDO.setTime(new Date());
+        stockInDO.setRepositoryID(1003);
+        stockInMapper.insert(stockInDO);
+
+    }
+}
